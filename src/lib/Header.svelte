b8b22b12f9e36c936102010ba7c169a989f3c43b
Candido Sales Gomes <candidosg@gmail.com>
2023-07-16 03:35:23 -0400

    fix header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -8,7 +8,6 @@
 <div class="absolute top-5 hidden w-full justify-between px-5 sm:flex">
     <a
         href="/"
-        target="_blank"
         class="w-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
         ><Cisco class="w-16 text-sky-400" />        
         </a
