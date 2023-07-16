43d71da616275190eecfbc6b6f30bd4b4e43fbf8
Candido Sales Gomes <candidosg@gmail.com>
2023-07-16 12:46:26 -0400

    fix header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,7 +4,7 @@
 	import Github from "./icons/Github.svelte";
 
 </script>
-<div class="absolute top-5 hidden w-full justify-between px-5 sm:flex">
+<div class="absolute top-5 w-full flex justify-between px-5 align-middle items-center">
     <a
         href="/"
         title="It isn't a official Cisco product"
@@ -14,7 +14,7 @@
     ><a
         href="https://github.com/candidosales/cisco-chat-frontend"
         target="_blank"
-        class="w-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+        class="md:block w-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
         ><Github /></a
     >
 </div>
